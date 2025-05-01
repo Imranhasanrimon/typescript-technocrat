d233be60254d7e1d2651246e8d59adfd6ccd0ffd
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-05-01 15:17:45 +0600

    learning how to define type in object and other data types

---
+++ module-1/src/1.4.ts
@@ -0,0 +1,8 @@
+let firstName: string = "Imran";
+
+let friends: string[] = ["Saad", "Najmul", "Ariful"]
+friends.push("Addur Rahman")
+// friends.push(25)
+
+let person1: [string, number, boolean, string] = ["Imran", 24, false, "Developer"];
+person1[0] = "25";
---
+++ module-1/src/1.5.ts
@@ -0,0 +1,12 @@
+const user: {
+    readonly companyName: string;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    isMarried?: boolean;
+} = {
+    companyName: "PH",
+    firstName: "imran",
+    middleName: "hasan",
+    lastName: "rimon",
+}
