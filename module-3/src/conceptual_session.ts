e86d656e45873e91076ceecc68360f00ac7b7487
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-05-07 14:30:10 +0600

    conceptual_session 2 of mission1

---
+++ module-3/src/conceptual_session.ts
@@ -0,0 +1,29 @@
+{
+    //SOLID-  Single Responsibility, Open/Closed, Liskov Substitution, Interface Segregation, Dependency Inversion
+
+    // DRY-Don't Repeat Yourself 
+    // KISS-Keep It Super Simple
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
